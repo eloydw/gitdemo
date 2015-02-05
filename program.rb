714147047835d9d6e143dcc66b014b39dff74b3e
Eloy GL <eloydw@gmail.com>
2015-02-05 12:23:34 +0100

    Le he añadido la clase persona

--- program.rb
+++ program.rb
@@ -1,1 +1,9 @@
-puts "hola mundo"
+class Person
+  attr_accessor :name, :surname, :age
+
+  def initialize(name, surname, age )
+    @name = name
+    @surname = surname
+    @age = edad
+  end
+end
