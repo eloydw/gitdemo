771417fc0d56c40399024e8f08b4e8538bcb7f8e
Eloy GL <eloydw@gmail.com>
2015-02-09 10:47:11 +0100

    person.rb

--- person.rb
+++ person.rb
@@ -1,9 +1,10 @@
 class Person
-  attr_accessor :name, :surname, :age
+  attr_accessor :name, :surname, :age, :phone
 
-  def initialize(name, surname, age )
+  def initialize(name, surname, age, phone )
     @name = name
     @surname = surname
     @age = edad
+    @phone = phone
   end
 end
